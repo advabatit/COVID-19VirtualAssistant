d8a7111fd6505e46c1aac9bcbb669dc4f4d57673
advabatit <advabatit@gmail.com>
2020-10-28 16:53:57 +0200

    UPDATE: removed the test function

--- WebScapingCOVID19.py
+++ WebScapingCOVID19.py
@@ -1,35 +1,8 @@
 from bs4 import BeautifulSoup 
-import xlsxwriter
 import requests
 import string
 
 URL = 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/coronavirus-disease-covid-19' 
-
-def create_excel() -> None:
-    """
-    Function that creates excel table with all the questions and the answers
-    Args:
-        Nothing
-    Return:
-        Nothing
-    """
-    questions, answers, last_update = sort_info()
-    excel_file = xlsxwriter.Workbook('questions_and_answeres.xlsx')
-    wsTransaction = excel_file.add_worksheet('questions_and_answeres')
-
-    row_num = 0
-    col_num = 0
-
-    wsTransaction.write(row_num, col_num, 'Questions')
-    wsTransaction.write(row_num, col_num + 1, 'Answers')
-
-    for i in range(len(questions)):
-        row_num += 1
-        wsTransaction.write(row_num, col_num, questions[i])
-        wsTransaction.write(row_num, col_num + 1, answers[i])
-
-    print("Finished!")
-    excel_file.close()
 
 
 def sort_info():
