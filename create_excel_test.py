3c76c23331d42f0dd778832efa2c94483b813a0b
advabatit <advabatit@gmail.com>
2020-10-28 16:54:59 +0200

    UPDATE: created file that meants for tests. the file execute .xlsx file with all the Q&A

---
+++ create_excel_test.py
@@ -0,0 +1,30 @@
+import xlsxwriter
+from WebScapingCOVID19 import sort_info
+
+def create_excel() -> None:
+    """
+    Function that creates excel table with all the questions and the answers
+    Args:
+        Nothing
+    Return:
+        Nothing
+    """
+    questions, answers, last_update = sort_info()
+    excel_file = xlsxwriter.Workbook('questions_and_answeres.xlsx')
+    wsTransaction = excel_file.add_worksheet('questions_and_answeres')
+
+    row_num = 0
+    col_num = 0
+
+    wsTransaction.write(row_num, col_num, 'Questions')
+    wsTransaction.write(row_num, col_num + 1, 'Answers')
+
+    for i in range(len(questions)):
+        row_num += 1
+        wsTransaction.write(row_num, col_num, questions[i])
+        wsTransaction.write(row_num, col_num + 1, answers[i])
+
+    print("Finished!")
+    excel_file.close()
+
+create_excel()
